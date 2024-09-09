0349850615c5e6586147c17b54b99b4be07ac826
Hiếu Huỳnh <hieu47471029@gmail.com>
2024-09-09 09:14:31 +0700

    first_commit

---
+++ src/layout/components/MenuRocker.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="nav-container primary-menu">
+        <div class="mobile-topbar-header">
+            <div>
+                <img src="../../assets/images/logo-icon.png" class="logo-icon" alt="logo icon">
+            </div>
+            <div>
+                <h4 class="logo-text">Rukada</h4>
+            </div>
+            <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+            </div>
+        </div>
+        <nav class="navbar navbar-expand-xl w-100">
+            <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
+                <li class="nav-item dropdown">
+                    <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
+                        data-bs-toggle="dropdown">
+                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        </div>
+                        <div class="menu-title">Thêm mới</div>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <router-link class="dropdown-item" to="/">
+                                <i class="bx bx-right-arrow-alt"></i> Bàn
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/phong-bida">
+                                <i class="bx bx-right-arrow-alt"></i> Phòng
+                            </router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/lich-lam">
+                                <i class="bx bx-right-arrow-alt"></i> Lịch làm
+                            </router-link>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/admin/nguyen-lieu">
+                        <div class="parent-icon"><i class="fa-solid fa-plate-wheat"></i></div>
+                        <div class="menu-title">Quản Lý khách hàng</div>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
